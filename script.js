15e1d5fbcea086914a873a8ab76e014bf923452e
Stephanie Hernandez <30811562+sgh005@users.noreply.github.com>
2020-08-02 19:17:18 -0700

    level1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var submitAnswer = function() {
+var submitAnswer = function(first,second, q1, q2, ques) {
 
     var radios = document.getElementsByName('choice');
     var val= "";
@@ -11,11 +11,39 @@ var submitAnswer = function() {
     
     if (val == "" ) {
       alert('please select choice answer');
-    } else if ( val == "Scripting" ) {
-      alert('Answer is correct !');
+    } else if ( val == "C" && first=='og' ) {
+        alert('Great Job!');
+        changeQuestion(first, second, q1, q2);
+       //trying to make modal work...not rn!
+        /* $(document).ready(function(){
+            $("#myBtn").click(function(){
+              $("#myModal").modal();
+            });
+          });
+        */
+    } else if (val == "A" & first=='new'){
+        alert('Great Job! ');
     } else {
       alert('Answer is wrong');
     }
+  };
+
+
+  var changeQuestion = function(first, second, question1, question2, ques ) {
+    var container = document.getElementById(first);
+    var newContainer = document.getElementById(second);
+
+    container.innerHTML = newContainer.innerHTML;
+
+    var q_container = document.getElementById(question1);
+    var q2_container = document.getElementById(question2);
+
+    q_container.innerHTML = q2_container.innerHTML;
+
+    if(ques==1){
+        ques=2;
+    }
+
   };
 
  
